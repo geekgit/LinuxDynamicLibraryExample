66915152cb95eefe63ecd9aa64b08e1e80a61044
geekgit <geeekz@gmail.com>
2016-01-05 20:50:43 +0500

    Sum function

--- lib.c
+++ lib.c
@@ -3,3 +3,7 @@ void HelloWorld()
 {
 	printf("Hello, World!\n");
 }
+int Sum(int A, int B)
+{
+	return A+B;
+}
--- start.c
+++ start.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <dlfcn.h>
-int main()
+void HelloWorld()
 {
 	void* Handle;
 	void (*HelloWorld)(void);
-	Handle = dlopen("libHello.so", RTLD_LAZY);
-	HelloWorld = dlsym(Handle, "HelloWorld");
+	Handle = dlopen ("libHello.so", RTLD_LAZY);
+	HelloWorld = dlsym (Handle, "HelloWorld");
 	HelloWorld();
-	dlclose(Handle);
+	dlclose (Handle);
+}
+void Sum(int A, int B)
+{
+	void* Handle;
+	int (*Sum)(int,int);
+	Handle = dlopen ("libHello.so", RTLD_LAZY);
+	Sum = dlsym (Handle, "Sum");
+	int C=Sum(A,B);
+	printf("%d+%d=%d\n",A,B,C);
+	dlclose (Handle);
+}
+int main()
+{
+	HelloWorld();
+	Sum(2,2);
+	Sum(100,1);
 }
